a5460a41b7dcdffda68260394a84178f25cd8557
Jeremy Le <jeremyle420@gmail.com>
2025-03-02 16:57:13 +1100

    feat(py-backend): complete task 1

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -3,23 +3,27 @@ from typing import List, Dict, Union
 from flask import Flask, request, jsonify
 import re
 
+
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
 class CookbookEntry:
-	name: str
+    name: str
+
 
 @dataclass
-class RequiredItem():
-	name: str
-	quantity: int
+class RequiredItem:
+    name: str
+    quantity: int
+
 
 @dataclass
 class Recipe(CookbookEntry):
-	required_items: List[RequiredItem]
+    required_items: List[RequiredItem]
+
 
 @dataclass
 class Ingredient(CookbookEntry):
-	cook_time: int
+    cook_time: int
 
 
 # =============================================================================
@@ -30,42 +34,67 @@ app = Flask(__name__)
 # Store your recipes here!
 cookbook = None
 
+
 # Task 1 helper (don't touch)
-@app.route("/parse", methods=['POST'])
+@app.route("/parse", methods=["POST"])
 def parse():
-	data = request.get_json()
-	recipe_name = data.get('input', '')
-	parsed_name = parse_handwriting(recipe_name)
-	if parsed_name is None:
-		return 'Invalid recipe name', 400
-	return jsonify({'msg': parsed_name}), 200
+    data = request.get_json()
+    recipe_name = data.get("input", "")
+    parsed_name = parse_handwriting(recipe_name)
+    if parsed_name is None:
+        return "Invalid recipe name", 400
+    return jsonify({"msg": parsed_name}), 200
+
 
 # [TASK 1] ====================================================================
-# Takes in a recipeName and returns it in a form that 
+# Takes in a recipeName and returns it in a human readable form
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+    """
+    Given a recipeName with potentially abysmal handwriting, return human readable string.
+
+    1. All hyphens and underscores are replaced with whitespace.
+    2. Food names can only contain letters and whitespace. All other characters are removed.
+    3. First character of every word should be capitalised.
+    4. Single whitespace between words. No leading and trailing whitespace.
+    5. Final string must be length > 0 characters.
+    """
+    # Maintain list of regex replacements
+    replacements = [
+        (r"[-_]", r" "),
+        (r"[^a-zA-Z ]", r""),
+        (r"\s+", " "),
+    ]
+
+    # Apply all regex replacements
+    for old, new in replacements:
+        recipeName = re.sub(old, new, recipeName)
+
+    # Apply upper case (title) formatting
+    recipeName = recipeName.strip().title()
+
+    # Return None is string is empty
+    return recipeName if recipeName else None
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
-@app.route('/entry', methods=['POST'])
+@app.route("/entry", methods=["POST"])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+    entry_data = request.get_json()
+    return "not implemented", 500
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
-@app.route('/summary', methods=['GET'])
+@app.route("/summary", methods=["GET"])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+    # TODO: implement me
+    return "not implemented", 500
 
 
 # =============================================================================
 # ==== DO NOT TOUCH ===========================================================
 # =============================================================================
 
-if __name__ == '__main__':
-	app.run(debug=True, port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
