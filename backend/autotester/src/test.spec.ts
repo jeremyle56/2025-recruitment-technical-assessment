061ab75a64c646a85060873b59fe611fd66c6d2a
Jeremy Le <jeremyle420@gmail.com>
2025-03-02 17:02:13 +1100

    feat(ts-backend): complete all tasks

--- backend/autotester/src/test.spec.ts
+++ backend/autotester/src/test.spec.ts
@@ -1,40 +1,38 @@
-const request = require("supertest");
+const request = require('supertest');
 
-describe("Task 1", () => {
-  describe("POST /parse", () => {
+describe('Task 1', () => {
+  describe('POST /parse', () => {
     const getTask1 = async (inputStr) => {
-      return await request("http://localhost:8080")
-        .post("/parse")
-        .send({ input: inputStr });
+      return await request('http://localhost:8080').post('/parse').send({ input: inputStr });
     };
 
-    it("example1", async () => {
-      const response = await getTask1("Riz@z RISO00tto!");
-      expect(response.body).toStrictEqual({ msg: "Rizz Risotto" });
+    it('example1', async () => {
+      const response = await getTask1('Riz@z RISO00tto!');
+      expect(response.body).toStrictEqual({ msg: 'Rizz Risotto' });
     });
 
-    it("example2", async () => {
-      const response = await getTask1("alpHa-alFRedo");
-      expect(response.body).toStrictEqual({ msg: "Alpha Alfredo" });
+    it('example2', async () => {
+      const response = await getTask1('alpHa-alFRedo');
+      expect(response.body).toStrictEqual({ msg: 'Alpha Alfredo' });
     });
 
-    it("error case", async () => {
-      const response = await getTask1("");
+    it('error case', async () => {
+      const response = await getTask1('');
       expect(response.status).toBe(400);
     });
   });
 });
 
-describe("Task 2", () => {
-  describe("POST /entry", () => {
+describe('Task 2', () => {
+  describe('POST /entry', () => {
     const putTask2 = async (data) => {
-      return await request("http://localhost:8080").post("/entry").send(data);
+      return await request('http://localhost:8080').post('/entry').send(data);
     };
 
-    it("Add Ingredients", async () => {
+    it('Add Ingredients', async () => {
       const entries = [
-        { type: "ingredient", name: "Egg", cookTime: 6 },
-        { type: "ingredient", name: "Lettuce", cookTime: 1 },
+        { type: 'ingredient', name: 'Egg', cookTime: 6 },
+        { type: 'ingredient', name: 'Lettuce', cookTime: 1 },
       ];
       for (const entry of entries) {
         const resp = await putTask2(entry);
@@ -43,52 +41,52 @@ describe("Task 2", () => {
       }
     });
 
-    it("Add Recipe", async () => {
+    it('Add Recipe', async () => {
       const meatball = {
-        type: "recipe",
-        name: "Meatball",
-        requiredItems: [{ name: "Beef", quantity: 1 }],
+        type: 'recipe',
+        name: 'Meatball',
+        requiredItems: [{ name: 'Beef', quantity: 1 }],
       };
       const resp1 = await putTask2(meatball);
       expect(resp1.status).toBe(200);
     });
 
-    it("Congratulations u burnt the pan pt2", async () => {
+    it('Congratulations u burnt the pan pt2', async () => {
       const resp = await putTask2({
-        type: "ingredient",
-        name: "beef",
+        type: 'ingredient',
+        name: 'beef',
         cookTime: -1,
       });
       expect(resp.status).toBe(400);
     });
 
-    it("Congratulations u burnt the pan pt3", async () => {
+    it('Congratulations u burnt the pan pt3', async () => {
       const resp = await putTask2({
-        type: "pan",
-        name: "pan",
+        type: 'pan',
+        name: 'pan',
         cookTime: 20,
       });
       expect(resp.status).toBe(400);
     });
 
-    it("Unique names", async () => {
+    it('Unique names', async () => {
       const resp = await putTask2({
-        type: "ingredient",
-        name: "Beef",
+        type: 'ingredient',
+        name: 'Beef',
         cookTime: 10,
       });
       expect(resp.status).toBe(200);
 
       const resp2 = await putTask2({
-        type: "ingredient",
-        name: "Beef",
+        type: 'ingredient',
+        name: 'Beef',
         cookTime: 8,
       });
       expect(resp2.status).toBe(400);
 
       const resp3 = await putTask2({
-        type: "recipe",
-        name: "Beef",
+        type: 'recipe',
+        name: 'Beef',
         cookTime: 8,
       });
       expect(resp3.status).toBe(400);
@@ -96,65 +94,63 @@ describe("Task 2", () => {
   });
 });
 
-describe("Task 3", () => {
-  describe("GET /summary", () => {
+describe('Task 3', () => {
+  describe('GET /summary', () => {
     const postEntry = async (data) => {
-      return await request("http://localhost:8080").post("/entry").send(data);
+      return await request('http://localhost:8080').post('/entry').send(data);
     };
 
     const getTask3 = async (name) => {
-      return await request("http://localhost:8080").get(
-        `/summary?name=${name}`
-      );
+      return await request('http://localhost:8080').get(`/summary?name=${name}`);
     };
 
-    it("What is bro doing - Get empty cookbook", async () => {
-      const resp = await getTask3("nothing");
+    it('What is bro doing - Get empty cookbook', async () => {
+      const resp = await getTask3('nothing');
       expect(resp.status).toBe(400);
     });
 
-    it("What is bro doing - Get ingredient", async () => {
+    it('What is bro doing - Get ingredient', async () => {
       const resp = await postEntry({
-        type: "ingredient",
-        name: "beef",
+        type: 'ingredient',
+        name: 'beef',
         cookTime: 2,
       });
       expect(resp.status).toBe(200);
 
-      const resp2 = await getTask3("beef");
+      const resp2 = await getTask3('beef');
       expect(resp2.status).toBe(400);
     });
 
-    it("Unknown missing item", async () => {
+    it('Unknown missing item', async () => {
       const cheese = {
-        type: "recipe",
-        name: "Cheese",
-        requiredItems: [{ name: "Not Real", quantity: 1 }],
+        type: 'recipe',
+        name: 'Cheese',
+        requiredItems: [{ name: 'Not Real', quantity: 1 }],
       };
       const resp1 = await postEntry(cheese);
       expect(resp1.status).toBe(200);
 
-      const resp2 = await getTask3("Cheese");
+      const resp2 = await getTask3('Cheese');
       expect(resp2.status).toBe(400);
     });
 
-    it("Bro cooked", async () => {
+    it('Bro cooked', async () => {
       const meatball = {
-        type: "recipe",
-        name: "Skibidi",
-        requiredItems: [{ name: "Bruh", quantity: 1 }],
+        type: 'recipe',
+        name: 'Skibidi',
+        requiredItems: [{ name: 'Bruh', quantity: 1 }],
       };
       const resp1 = await postEntry(meatball);
       expect(resp1.status).toBe(200);
 
       const resp2 = await postEntry({
-        type: "ingredient",
-        name: "Bruh",
+        type: 'ingredient',
+        name: 'Bruh',
         cookTime: 2,
       });
       expect(resp2.status).toBe(200);
 
-      const resp3 = await getTask3("Skibidi");
+      const resp3 = await getTask3('Skibidi');
       expect(resp3.status).toBe(200);
     });
   });
--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,22 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 // ==== Type Definitions, feel free to add or modify ==========================
-interface cookbookEntry {
-  name: string;
+interface CookbookEntry {
   type: string;
 }
 
-interface requiredItem {
+interface RequiredItem {
   name: string;
   quantity: number;
 }
 
-interface recipe extends cookbookEntry {
-  requiredItems: requiredItem[];
+interface Recipe extends CookbookEntry {
+  requiredItems: RequiredItem[];
 }
 
-interface ingredient extends cookbookEntry {
+interface Ingredient extends CookbookEntry {
   cookTime: number;
+}
+
+interface RecipeSummary {
+  name: string;
+  cookTime: number;
+  ingredients: RequiredItem[];
 }
 
 // =============================================================================
@@ -26,43 +31,164 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: Record<string, Recipe | Ingredient> = {};
 
 // Task 1 helper (don't touch)
-app.post("/parse", (req:Request, res:Response) => {
+app.post('/parse', (req: Request, res: Response) => {
   const { input } = req.body;
 
-  const parsed_string = parse_handwriting(input)
+  const parsed_string = parse_handwriting(input);
   if (parsed_string == null) {
-    res.status(400).send("this string is cooked");
+    res.status(400).send('this string is cooked');
     return;
-  } 
+  }
   res.json({ msg: parsed_string });
   return;
-  
 });
 
 // [TASK 1] ====================================================================
-// Takes in a recipeName and returns it in a form that 
+// Takes in a recipeName and returns it in a human readable form
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
-}
+  /*
+    Maintain list of regex replacements ([pattern, replacement])
+    1. Replacing all hyphens and underscores with a whitespace
+    2. Removing all non letters and non whitespace
+    3. Squash all whitespace to single whitespace
+  */
+  const replacements: [RegExp, string][] = [
+    [/[-_]/g, ' '],
+    [/[^a-zA-Z\s]/g, ''],
+    [/\s+/g, ' '],
+  ];
+
+  // Apply all regex replacements
+  for (let [pattern, replacement] of replacements) {
+    recipeName = recipeName.replace(pattern, replacement);
+  }
+
+  // Trim leading and trailing whitespace
+  recipeName = recipeName.trim();
+
+  // Capitalise first character of every word
+  recipeName = recipeName.toLowerCase();
+  recipeName = recipeName.replace(/\b\w/g, (m: string) => m.toUpperCase());
+
+  return recipeName ? recipeName : null;
+};
 
 // [TASK 2] ====================================================================
-// Endpoint that adds a CookbookEntry to your magical cookbook
-app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
 
+const ERROR = 400;
+const SUCCESS = 200;
+
+/**
+ * Cookbook entry validator. Helper function for POST /entry.
+ *
+ * @param type - recipe or ingredient
+ * @param name - name of item
+ * @param requiredItems - array of required items for recipe
+ * @param cookTime - integer for cook time for ingredients
+ * @returns SUCCESS or ERROR code and message
+ */
+const entryValidator = (
+  type: string,
+  name: string,
+  requiredItems: RequiredItem[],
+  cookTime: number
+): [Number, string] => {
+  // Check type is either 'recipe' or 'ingredient'
+  if (!['recipe', 'ingredient'].includes(type))
+    return [ERROR, 'type can only be "recipe" or "ingredient".'];
+
+  // Check entry names must be unique
+  if (name in cookbook) return [ERROR, 'entry already exists in cookbook'];
+
+  // Check uniqueness of requiredItems in recipes
+  if (type === 'recipe' && new Set(requiredItems.map((i) => i.name)).size !== requiredItems.length)
+    return [ERROR, 'Recipe requiredItems must be unique names'];
+
+  // Check cook time greater or equal to 0
+  if (type === 'ingredient' && !(typeof cookTime === 'number' && cookTime >= 0))
+    return [ERROR, 'cookTime must be greater or equal to 0'];
+
+  return [SUCCESS, ''];
+};
+
+// Endpoint that adds a CookbookEntry to your magical cookbook
+app.post('/entry', (req: Request, res: Response) => {
+  const { type, name, requiredItems, cookTime } = req.body;
+
+  // Validate cookbook entry
+  const [code, message] = entryValidator(type, name, requiredItems, cookTime);
+
+  // If successful, add entry to the cookbook
+  if (code === SUCCESS) {
+    cookbook[name] = type === 'recipe' ? { type, requiredItems } : { type, cookTime };
+    return res.status(code).json({});
+  }
+
+  res.status(code).json({ message });
 });
 
 // [TASK 3] ====================================================================
-// Endpoint that returns a summary of a recipe that corresponds to a query name
-app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
 
+/**
+ * Recursive function to collect required items and cook time. Helper for GET /summary.
+ *
+ * @param summary - summary object to be returned
+ * @param recipe - current recipe
+ * @param quantity - amount required for current recipe
+ * @returns - summary object for GET request
+ */
+const createSummary = (
+  summary: RecipeSummary,
+  recipe: Recipe,
+  quantity: number
+): RecipeSummary | null => {
+  // Iterate through required items of current recipe
+  for (const { name: itemName, quantity: itemQty } of recipe.requiredItems) {
+    // Check recipe contains items that are in the cookbook
+    if (!(itemName in cookbook)) return null;
+
+    // If item is recipe, call the function recursively. If ingredient, add to summary object.
+    if (cookbook[itemName].type === 'recipe')
+      summary = createSummary(summary, cookbook[itemName] as Recipe, itemQty);
+    else {
+      // Total Quantity = current required ingredient * quantity of recipe
+      const totalQty = itemQty * quantity;
+
+      // If item already exists in summary object, add quantity to that object
+      const existing = summary.ingredients.find((i) => i.name === itemName);
+      if (existing) existing.quantity += totalQty;
+      else summary.ingredients.push({ name: itemName, quantity: totalQty });
+
+      // Increase total cook time based on ingredient cook time and quantity
+      summary.cookTime += (cookbook[itemName] as Ingredient).cookTime * totalQty;
+    }
+  }
+
+  return summary;
+};
+
+// Endpoint that returns a summary of a recipe that corresponds to a query name
+app.get('/summary', (req: Request, res: Request) => {
+  const { name } = req.query;
+
+  // Check if recipe with corresponding name is in the cookbook
+  if (!(name in cookbook)) return res.status(ERROR).send(`Recipe '${name}'f cannot be found.`);
+
+  // Check if searched name is a recipe
+  if (cookbook[name].type !== 'recipe')
+    return res.status(ERROR).send(`Searched name: ${name} is not a recipe name.`);
+
+  // Create summary object
+  const summary = createSummary(
+    { name, cookTime: 0, ingredients: [] },
+    cookbook[name] as Recipe,
+    1
+  );
+
+  res.status(summary ? SUCCESS : ERROR).json(summary);
 });
 
 // =============================================================================
